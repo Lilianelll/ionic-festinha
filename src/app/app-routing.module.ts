82a2d21667051bdbf8475218bb5f0ab82acbf506
Dieme <diememartins19@gmail.com>
2019-07-01 09:02:38 -0300

    inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  //{ path: 'add-usuario', loadChildren: './pages/add-usuario/add-usuario.module#AddUsuarioPageModule' },
+  //{ path: 'list-usuario', loadChildren: './pages/list-usuario/list-usuario.module#ListUsuarioPageModule' },
+ // { path: 'usuario', loadChildren: './model/usuario/usuario.module#UsuarioPageModule' }
 ];
 @NgModule({
   imports: [
